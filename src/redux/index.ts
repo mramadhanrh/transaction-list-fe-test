9b22fb08a6609017b98a54819109a7c8e641b55a
Ramadhan Rahmat <ramadhan.rahmat@stockbit.com>
2022-05-21 08:52:38 +0700

    Configure redux

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,4 +1,4 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
   reducer: combineReducers({}),

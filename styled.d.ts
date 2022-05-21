be047a201395308c7b76c8e964abbb85b07bc59d
Ramadhan Rahmat <ramadhan.rahmat@stockbit.com>
2022-05-21 12:19:09 +0700

    Add new color variant

--- styled.d.ts
+++ styled.d.ts
@@ -1,7 +1,15 @@
-import "styled-components";
+import 'styled-components';
 
-declare module "styled-components" {
-  export type ColorVariant = "cloud" | "green" | "orange";
+declare module 'styled-components' {
+  export type ColorVariant =
+    | 'cloud'
+    | 'green'
+    | 'orange'
+    | 'linen'
+    | 'lightgray'
+    | 'platinum'
+    | 'white'
+    | 'black';
 
   export interface DefaultTheme {
     color: Record<ColorVariant, string>;

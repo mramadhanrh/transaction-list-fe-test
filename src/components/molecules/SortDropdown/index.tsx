755e5a347b961177d9a6f7506fdef2a126b718d8
Ramadhan Rahmat <ramadhan.rahmat@stockbit.com>
2022-05-21 12:20:08 +0700

    Add SortDropdown component

---
+++ src/components/molecules/SortDropdown/index.tsx
@@ -0,0 +1,44 @@
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FC, useRef, useState } from 'react';
+import useOutsideClickAlert from '../../hooks/useOutsideClickAlert';
+import { ActiveItem, Container, ContentItem, ContentList } from './style';
+
+interface SortDropdownProps {
+  data?: string[];
+  onSelect?(index: number): void;
+}
+
+const SortDropdown: FC<SortDropdownProps> = ({
+  data = [],
+  onSelect = () => {},
+}) => {
+  const [active, setActive] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => setActive(!active);
+
+  useOutsideClickAlert(() => {
+    setActive(false);
+  }, dropdownRef);
+
+  return (
+    <Container ref={dropdownRef} onClick={toggleDropdown}>
+      <ActiveItem>
+        URUTKAN
+        <span>
+          <FontAwesomeIcon icon={faChevronDown} />
+        </span>
+      </ActiveItem>
+      <ContentList active={active}>
+        {data.map((item, i) => (
+          <ContentItem key={i.toString()} onClick={() => onSelect(i)}>
+            {item}
+          </ContentItem>
+        ))}
+      </ContentList>
+    </Container>
+  );
+};
+
+export default SortDropdown;

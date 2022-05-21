f6b229bb7fa449c08f9c91d8dcd6b7f3587ab746
Ramadhan Rahmat <ramadhan.rahmat@stockbit.com>
2022-05-21 08:52:24 +0700

    Integrate Theme and globalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from "react";
-import { Provider } from "react-redux";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { store } from "./redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import { store } from './redux';
+import GlobalStyle from './GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './constants/theme';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 

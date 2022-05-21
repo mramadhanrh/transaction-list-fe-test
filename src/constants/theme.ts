46c7de17cce6278e4c3f0e5db013dd6df64d85c4
Ramadhan Rahmat <ramadhan.rahmat@stockbit.com>
2022-05-21 08:52:54 +0700

    Configure theme values

---
+++ src/constants/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components';
+
+const defaultTheme: DefaultTheme = {
+  color: {
+    cloud: '#f3f7f9',
+    green: '#56b586',
+    orange: '#fd6542',
+  },
+};
+
+export default defaultTheme;

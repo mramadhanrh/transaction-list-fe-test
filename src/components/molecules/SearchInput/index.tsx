2ca252d2767e64a9edd6d836af043d2203b98789
Ramadhan Rahmat <ramadhan.rahmat@stockbit.com>
2022-05-21 08:54:24 +0700

    Init SearchInput and TransactionItem component

---
+++ src/components/molecules/SearchInput/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const SearchInput: FC = () => {
+  return <input />;
+};
+
+export default SearchInput;
---
+++ src/components/molecules/TransactionItem/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import Button from '../../atoms/Button';
+
+const TransactionItem: FC = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+};
+
+export default TransactionItem;
